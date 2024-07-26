1bee85b8f8d80a22d298bf54f2f80f215bd274a4
javohir <ajavohir969@gmail.com>
2024-07-26 16:13:04 +0500

    najot talim imtixon

---
+++ js/app.js
@@ -0,0 +1,274 @@
+
+const btn_star = document.querySelector(".menu_card_btn_star");
+const star = document.querySelector("#star");
+
+const btn_star2 = document.querySelector(".menu_card_btn_star2");
+const star2 = document.querySelector("#star2");
+
+const btn_star3 = document.querySelector(".menu_card_btn_star3");
+const star3 = document.querySelector("#star3");
+
+const btn_star4 = document.querySelector(".menu_card_btn_star4");
+const star4 = document.querySelector("#star4");
+
+const btn_star5 = document.querySelector(".menu_card_btn_star5");
+const star5 = document.querySelector("#star5");
+
+
+//card2
+
+const card2_btn_star = document.querySelector(".menu_card2_btn_star");
+const card2_star = document.querySelector("#card2_star");
+
+const card2_btn_star2 = document.querySelector(".menu_card2_btn_star2");
+const card2_star2 = document.querySelector("#card2_star2");
+
+const card2_btn_star3 = document.querySelector(".menu_card2_btn_star3");
+const card2_star3 = document.querySelector("#card2_star3");
+
+const card2_btn_star4 = document.querySelector(".menu_card2_btn_star4");
+const card2_star4 = document.querySelector("#card2_star4");
+
+const card2_btn_star5 = document.querySelector(".menu_card2_btn_star5");
+const card2_star5 = document.querySelector("#card2_star5");
+
+//card3
+
+const card3_btn_star = document.querySelector(".menu_card3_btn_star");
+const card3_star = document.querySelector("#card3_star");
+
+const card3_btn_star2 = document.querySelector(".menu_card3_btn_star2");
+const card3_star2 = document.querySelector("#card3_star2");
+
+const card3_btn_star3 = document.querySelector(".menu_card3_btn_star3");
+const card3_star3 = document.querySelector("#card3_star3");
+
+const card3_btn_star4 = document.querySelector(".menu_card3_btn_star4");
+const card3_star4 = document.querySelector("#card3_star4");
+
+const card3_btn_star5 = document.querySelector(".menu_card3_btn_star5");
+const card3_star5 = document.querySelector("#card3_star5");
+
+//card4
+
+const card4_btn_star = document.querySelector(".menu_card4_btn_star");
+const card4_star = document.querySelector("#card4_star");
+
+const card4_btn_star2 = document.querySelector(".menu_card4_btn_star2");
+const card4_star2 = document.querySelector("#card4_star2");
+
+const card4_btn_star3 = document.querySelector(".menu_card4_btn_star3");
+const card4_star3 = document.querySelector("#card4_star3");
+
+const card4_btn_star4 = document.querySelector(".menu_card4_btn_star4");
+const card4_star4 = document.querySelector("#card4_star4");
+
+const card4_btn_star5 = document.querySelector(".menu_card4_btn_star5");
+const card4_star5 = document.querySelector("#card4_star5");
+
+//card5
+
+const card5_btn_star = document.querySelector(".menu_card5_btn_star");
+const card5_star = document.querySelector("#card5_star");
+
+const card5_btn_star2 = document.querySelector(".menu_card5_btn_star2");
+const card5_star2 = document.querySelector("#card5_star2");
+
+const card5_btn_star3 = document.querySelector(".menu_card5_btn_star3");
+const card5_star3 = document.querySelector("#card5_star3");
+
+const card5_btn_star4 = document.querySelector(".menu_card5_btn_star4");
+const card5_star4 = document.querySelector("#card5_star4");
+
+const card5_btn_star5 = document.querySelector(".menu_card5_btn_star5");
+const card5_star5 = document.querySelector("#card5_star5");
+
+//card6
+
+const card6_btn_star = document.querySelector(".menu_card6_btn_star");
+const card6_star = document.querySelector("#card6_star");
+
+const card6_btn_star2 = document.querySelector(".menu_card6_btn_star2");
+const card6_star2 = document.querySelector("#card6_star2");
+
+const card6_btn_star3 = document.querySelector(".menu_card6_btn_star3");
+const card6_star3 = document.querySelector("#card6_star3");
+
+const card6_btn_star4 = document.querySelector(".menu_card6_btn_star4");
+const card6_star4 = document.querySelector("#card6_star4");
+
+const card6_btn_star5 = document.querySelector(".menu_card6_btn_star5");
+const card6_star5 = document.querySelector("#card6_star5");
+
+//testimonial
+
+const testimonial = document.querySelector(".testimonial_btn_star");
+const testimonialstar = document.querySelector("#testimonial_star");
+
+const testimonial2 = document.querySelector(".testimonial_btn_star2");
+const testimonialstar2 = document.querySelector("#testimonial_star2");
+
+const testimonial3 = document.querySelector(".testimonial_btn_star3");
+const testimonialstar3 = document.querySelector("#testimonial_star3");
+
+const testimonial4 = document.querySelector(".testimonial_btn_star4");
+const testimonialstar4 = document.querySelector("#testimonial_star4");
+
+const testimonial5 = document.querySelector(".testimonial_btn_star5");
+const testimonialstar5 = document.querySelector("#testimonial_star5");
+
+
+
+//card1
+
+btn_star.addEventListener("click", () => {
+    star.style.color = "#DC780B"
+});
+
+btn_star2.addEventListener("click", () => {
+    star2.style.color = "#DC780B"
+});
+
+btn_star3.addEventListener("click", () => {
+    star3.style.color = "#DC780B"
+});
+
+btn_star4.addEventListener("click", () => {
+    star4.style.color = "#DC780B"
+});
+
+btn_star5.addEventListener("click", () => {
+    star5.style.color = "#DC780B"
+});
+
+//card2
+
+card2_btn_star.addEventListener("click", () => {
+    card2_star.style.color = "#DC780B"
+});
+
+card2_btn_star2.addEventListener("click", () => {
+    card2_star2.style.color = "#DC780B"
+});
+
+card2_btn_star3.addEventListener("click", () => {
+    card2_star3.style.color = "#DC780B"
+});
+
+card2_btn_star4.addEventListener("click", () => {
+    card2_star4.style.color = "#DC780B"
+});
+
+card2_btn_star5.addEventListener("click", () => {
+    card2_star5.style.color = "#DC780B"
+});
+
+//card3
+
+card3_btn_star.addEventListener("click", () => {
+    card3_star.style.color = "#DC780B"
+});
+
+card3_btn_star2.addEventListener("click", () => {
+    card3_star2.style.color = "#DC780B"
+});
+
+card3_btn_star3.addEventListener("click", () => {
+    card3_star3.style.color = "#DC780B"
+});
+
+card3_btn_star4.addEventListener("click", () => {
+    card3_star4.style.color = "#DC780B"
+});
+
+card3_btn_star5.addEventListener("click", () => {
+    card3_star5.style.color = "#DC780B"
+});
+
+//card4
+
+card4_btn_star.addEventListener("click", () => {
+    card4_star.style.color = "#DC780B"
+});
+
+card4_btn_star2.addEventListener("click", () => {
+    card4_star2.style.color = "#DC780B"
+});
+
+card4_btn_star3.addEventListener("click", () => {
+    card4_star3.style.color = "#DC780B"
+});
+
+card4_btn_star4.addEventListener("click", () => {
+    card4_star4.style.color = "#DC780B"
+});
+
+card4_btn_star5.addEventListener("click", () => {
+    card4_star5.style.color = "#DC780B"
+});
+
+//card5
+
+card5_btn_star.addEventListener("click", () => {
+    card5_star.style.color = "#DC780B"
+});
+
+card5_btn_star2.addEventListener("click", () => {
+    card5_star2.style.color = "#DC780B"
+});
+
+card5_btn_star3.addEventListener("click", () => {
+    card5_star3.style.color = "#DC780B"
+});
+
+card5_btn_star4.addEventListener("click", () => {
+    card5_star4.style.color = "#DC780B"
+});
+
+card5_btn_star5.addEventListener("click", () => {
+    card5_star5.style.color = "#DC780B"
+});
+
+//card6
+
+card6_btn_star.addEventListener("click", () => {
+    card6_star.style.color = "#DC780B"
+});
+
+card6_btn_star2.addEventListener("click", () => {
+    card6_star2.style.color = "#DC780B"
+});
+
+card6_btn_star3.addEventListener("click", () => {
+    card6_star3.style.color = "#DC780B"
+});
+
+card6_btn_star4.addEventListener("click", () => {
+    card6_star4.style.color = "#DC780B"
+});
+
+card6_btn_star5.addEventListener("click", () => {
+    card6_star5.style.color = "#DC780B"
+});
+
+//testimonial
+
+testimonial.addEventListener("click", () => {
+    testimonialstar.style.color = "#DC780B"
+});
+
+testimonial2.addEventListener("click", () => {
+    testimonialstar2.style.color = "#DC780B"
+});
+
+testimonial3.addEventListener("click", () => {
+    testimonialstar3.style.color = "#DC780B"
+});
+
+testimonial4.addEventListener("click", () => {
+    testimonialstar4.style.color = "#DC780B"
+});
+
+testimonial5.addEventListener("click", () => {
+    testimonialstar5.style.color = "#DC780B"
+});
